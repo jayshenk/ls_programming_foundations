2fb66c8f0ae20270833e02666a75f72ed1124c99
Jay Shenk <jay.shenk@gmail.com>
2016-01-27 21:16:33 -0500

    Add Computer AI Offense

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -50,8 +50,8 @@ def joinor(arr, delimiter=', ', word='or')
   arr.join(delimiter)
 end
 
-def find_at_risk_square(line, brd)
-  if brd.values_at(*line).count(PLAYER_MARKER) == 2
+def find_at_risk_square(line, brd, marker)
+  if brd.values_at(*line).count(marker) == 2
     brd.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.sample
   else
     nil
@@ -72,11 +72,22 @@ end
 
 def computer_places_piece!(brd)
   square = nil
+
+  # defense first
   WINNING_LINES.each do |line|
-    square = find_at_risk_square(line, brd)
+    square = find_at_risk_square(line, brd, PLAYER_MARKER)
     break if square
   end
 
+  # offense
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+      break if square
+    end
+  end
+
+  # just pick a square
   if !square
     square = empty_squares(brd).sample
   end
